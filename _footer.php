87d4c3f5f9f849396d73fb82d9e6cf990c85a135
Trevor Kimenye <kimenye@gmail.com>
2015-10-11 17:34:22 +0300

    Began work on footer

---
+++ _footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="strip"></div>
+  <div class="info">
+  </div>
+  <div class="copyright ui page grid">
+    <p>Copyright <?php echo date('Y'); ?> &copy; Kenya Paediatric Association</p>
+  </div>
+</footer>
--- index.php
+++ index.php
@@ -3,3 +3,8 @@
   $title = 'Kenya Paediatric Association - Afya ya watoto wetu';
   include '_header.php';
 ?>
+
+<div class="hero">
+</div>
+
+<?php include '_footer.php'; ?>
